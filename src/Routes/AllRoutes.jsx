58ffe5996f676cbaa2ebbc0b0f271c2cc31ff731
Roshan Arikar <85448130+roshanarikar@users.noreply.github.com>
2022-09-08 12:14:26 +0530

    routes added

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,5 +1,14 @@
 import { Route, Routes } from "react-router-dom"
-
+import Home from "../Components/Home/home"
+import { SignIn } from "../Components/SignIn/SignIn"
+import { Login } from '../Components/SignIn/Login';
+import { Cleansers } from '../Components/ProductPages/Cleansers';
+import { DryFruits } from '../Components/ProductPages/DryFruits';
+import { FacePrime } from '../Components/ProductPages/FacePrime';
+import { ProductsEyes } from '../Components/ProductPages/ProductsEyes';
+import { ProductDetails } from '../Components/productDetails/ProductDetails';
+import { Checkout } from '../Components/Checkout/CheckOut'
+import { ThankYou } from '../Components/ThankYou/ThankYou'
 
 
 export const AllRoutes = () =>{
@@ -7,15 +16,15 @@ export const AllRoutes = () =>{
         <div>
             <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/signin" element={<SignIn />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/cleansers" element={<Cleansers />}></Route>
+        <Route path="/signin" element={<SignIn/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/cleansers" element={<Cleansers/>}></Route>
         <Route path="/dryfruits" element={<DryFruits />}></Route>
         <Route path="/faceprime" element={<FacePrime />}></Route>
         <Route path="/productseyes" element={<ProductsEyes />}></Route>
         <Route path="/productsdetails" element={<ProductDetails />}></Route>
         <Route path="/checkout" element={<Checkout />}></Route>
-        <Route path="/thankyou" element={<ThankYou />}></Route>
+        <Route path="/thankyou" element={<ThankYou/>}></Route>
       </Routes>
         </div>
     )
